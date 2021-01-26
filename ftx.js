cb99418069c5a015422e9c86ffa8cb323c0e1d36
Mike Reilly <reillysemail@gmail.com>
2021-01-26 15:31:21 -0800

    added env file

--- ftx.js
+++ ftx.js
@@ -1,8 +1,18 @@
 const axios = require('axios');
+require('dotenv').config();
+
+const ts = Date.now();
 
 const main = async () => {
-
-
+    const resp = await axios({
+        method: 'get',
+        url: 'https://ftx.com',
+        headers: {
+            'FTX-KEY': process.env.FTX_KEY,
+        },
+    });
+    console.log(resp.status);
+    console.log(resp.data);
 };
 
 main()
