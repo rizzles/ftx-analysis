7d72b09be304e450afddd5bf4eef22a91c22acba
Mike Reilly <reillysemail@gmail.com>
2021-01-26 15:59:27 -0800

    working request

--- ftx.js
+++ ftx.js
@@ -1,14 +1,23 @@
 const axios = require('axios');
+const CryptoJS = require('crypto-js');
 require('dotenv').config();
 
 const ts = Date.now();
+const method = 'GET';
+const path = '/markets/BTC-PERP';
 
 const main = async () => {
+    const payload = `${ts}${method}${path}`;
+    const signature = CryptoJS.HmacSHA256(process.env.FTX_PRIV_KEY, payload)
+        .toString(CryptoJS.enc.Hex);
+
     const resp = await axios({
         method: 'get',
-        url: 'https://ftx.com',
+        url: `https://ftx.com/api${path}`,
         headers: {
             'FTX-KEY': process.env.FTX_KEY,
+            'FTX-TX': ts.toString(),
+            'FTX-SIGN': signature,
         },
     });
     console.log(resp.status);
