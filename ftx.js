cd31fea32227de4d3014a2d44b4c39c106401ad6
Mike Reilly <reillysemail@gmail.com>
2021-01-26 15:19:42 -0800

    Added files

---
+++ ftx.js
@@ -0,0 +1,13 @@
+const axios = require('axios');
+
+const main = async () => {
+
+
+};
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
